3a1fcb85c134128cc7d70beb6ad4e5baae6a4d2c
stzhyegeun <hyegeun.cho@sundaytoz.com>
2017-02-16 13:28:01 +0900

    [StzLog] assert 함수 추가

--- libs/StzCommon/StzLog.js
+++ libs/StzCommon/StzLog.js
@@ -8,6 +8,17 @@ define(function () {
 			console.log(inValue);
 		}
 	}
+
+	// REFER http://stackoverflow.com/questions/15313418/javascript-assert
+	StzLog.prototype.assert = function(condition, message) {
+		if (!condition) {
+			message = message || "Assertion failed";
+			if (typeof Error !== "undefined") {
+				throw new Error(message);
+			}
+			throw message; // Fallback
+		}
+	}
 	
 	return StzLog;
 });
