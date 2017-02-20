6b57211c40373933d72a102bcbb59b00ad1174c3
YooYoungSun <youngsun.yoo@sundaytoz.com>
2017-02-20 22:20:41 +0900

    [사천성] 월드맵 작업중

--- Sachunsung/WebContent/js/Worldmap.js
+++ Sachunsung/WebContent/js/Worldmap.js
@@ -5,8 +5,12 @@ function Worldmap() {
 }
 
 /** @type Phaser.State */
+var worldmapBmd;
+
 var proto = Object.create(Phaser.State);
 Worldmap.prototype = proto;
+
+var speedMult = 0.7;
 
 var cursors;
 
@@ -15,30 +19,78 @@ Worldmap.prototype.preload = function() {
 };
 
 Worldmap.prototype.create = function() {
-	this.game.world.setBounds(0, 0, 0, 2000);
-	
-	this.game.add.sprite(0, 480, 'worldmap1');
-	
-	 cursors = this.game.input.keyboard.createCursorKeys();
+
+	this.makeWorldMap();
+
+	cursors = this.game.input.keyboard.createCursorKeys();
+	 
+	this.makeButton();
 };
 
 Worldmap.prototype.update = function() {
-
-    if (cursors.left.isDown)
-    {
-    	this.game.camera.x -= 4;
-    }
-    else if (cursors.right.isDown)
-    {
-    	this.game.camera.x += 4;
-    }
-
+	
     if (cursors.up.isDown)
-    {
-    	this.game.camera.y -= 4;
+    {    	
+    	if(this.scrollingMap.y - 5 < -403*(repeatConut-2)){
+    		return;
+    	}
+    	
+    	this.scrollingMap.y -= 4;
     }
     else if (cursors.down.isDown)
     {
-    	this.game.camera.y += 4;
+    	this.scrollingMap.y += 4;
     }
-}
+
+	 //this.game.y += 5;
+	 //this.mountainsBack.tilePosition.y += 5;
+};
+
+var repeatConut;
+
+Worldmap.prototype.makeWorldMap = function() {
+	
+	repeatConut = StzGameConfig.TOTAL_EPISODE_COUNT/4;
+	var temp = this.game.make.bitmapData(this.game.width, 403);
+	worldmapBmd = this.game.make.bitmapData(this.game.width, 403*(repeatConut));
+
+	for(var i =5; i < repeatConut - 4; i++){
+		var str = 'worldmap'+(5+((i-1)%4));
+		temp.copy(str);
+		worldmapBmd.draw(temp,0,  403*(repeatConut-i));
+	}
+	
+	temp.copy('worldmap4');
+	worldmapBmd.draw(temp,0,  403*(repeatConut-4));
+
+	temp.copy('worldmap3');
+	
+	worldmapBmd.draw(temp,0,  403*(repeatConut-3));
+	
+	temp.copy('worldmap2');
+	
+	worldmapBmd.draw(temp,0,  403*(repeatConut-2));
+	
+	temp.copy('worldmap1');
+	
+	worldmapBmd.draw(temp,0,  403*(repeatConut-1));
+	
+	
+	this.scrollingMap = this.game.add.image(0, 0, worldmapBmd);
+	this.scrollingMap.y = -403*(repeatConut-2);
+};
+
+Worldmap.prototype.makeButton = function() {
+
+	var button = this.game.add.button(150, 403*(repeatConut-2), 'btnStage', null, this, 'disableStage.png', 'disableStage.png', 'disableStage.png');
+	
+	var button1 = this.game.add.button(150, 100, 'disableStage.png', 
+			null, this, 2, 1, 0);
+	
+	var button2 = this.game.add.button(200, 200, 'disableStage.png', 
+			null, this, 2, 1, 0);
+	
+	this.scrollingMap.addChild(button);
+	this.scrollingMap.addChild(button1);
+	this.scrollingMap.addChild(button2);
+};
