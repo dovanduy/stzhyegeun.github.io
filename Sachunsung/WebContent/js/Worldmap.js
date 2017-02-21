896088482012a0c57ad253983accf49cc738f2b1
YooYoungSun <youngsun.yoo@sundaytoz.com>
2017-02-21 12:35:05 +0900

    [사천성] 월드맵 드래그 기능 추가

--- Sachunsung/WebContent/js/Worldmap.js
+++ Sachunsung/WebContent/js/Worldmap.js
@@ -12,6 +12,7 @@ var proto = Object.create(Phaser.State);
 Worldmap.prototype = proto;
 
 var speedMult = 0.7;
+var friction = 0.99;
 
 var cursors;
 
@@ -30,21 +31,71 @@ Worldmap.prototype.create = function() {
 
 Worldmap.prototype.update = function() {
 	
-    if (cursors.up.isDown)
-    {    	
-    	if(this.scrollingMap.y - 5 < -403*(repeatConut-2)){
-    		return;
-    	}
-    	
-    	this.scrollingMap.y -= 4;
-    }
-    else if (cursors.down.isDown)
-    {
-    	this.scrollingMap.y += 4;
-    }
+//    if (cursors.up.isDown)
+//    {    	
+//    	if(this.scrollingMap.y - 5 < -403*(repeatConut-2)){
+//    		return;
+//    	}
+//    	
+//    	this.scrollingMap.y -= 4;
+//    }
+//    else if (cursors.down.isDown)
+//    {
+//    	this.scrollingMap.y += 4;
+//    }
 
 	 //this.game.y += 5;
 	 //this.mountainsBack.tilePosition.y += 5;
+	
+	 if(this.scrollingMap.isBeingDragged){
+         // save current map position
+         this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+    }
+	 else{
+         // if the moving speed is greater than 1...
+         if(this.scrollingMap.movingSpeed > 1){
+              // adjusting map x position according to moving speed and angle using trigonometry
+              this.scrollingMap.x += this.scrollingMap.movingSpeed * Math.cos(this.scrollingMap.movingangle);
+              // adjusting map y position according to moving speed and angle using trigonometry
+              this.scrollingMap.y += this.scrollingMap.movingSpeed * Math.sin(this.scrollingMap.movingangle);
+              // keep map within boundaries
+              if(this.scrollingMap.x < this.game.width - this.scrollingMap.width){
+                   this.scrollingMap.x = this.game.width - this.scrollingMap.width;
+              }
+              // keep map within boundaries
+              if(this.scrollingMap.x > 0){
+                   this.scrollingMap.x = 0;
+              }
+              // keep map within boundaries
+              if(this.scrollingMap.y < this.game.height - this.scrollingMap.height){
+                   this.scrollingMap.y = this.game.height - this.scrollingMap.height;
+              }
+              // keep map within boundaries
+              if(this.scrollingMap.y > 0){
+                   this.scrollingMap.y = 0;
+              }
+              // applying friction to moving speed
+              this.scrollingMap.movingSpeed *= friction;
+              // save current map position
+              this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+         }
+         // if the moving speed is less than 1...
+         
+         else{
+              // checking distance between current map position and last saved position
+              // which is the position in the previous frame
+              var distance = this.scrollingMap.savedPosition.distance(this.scrollingMap.position);
+              // same thing with the angle
+              var angle = this.scrollingMap.savedPosition.angle(this.scrollingMap.position);
+              // if the distance is at least 4 pixels (an arbitrary value to see I am swiping)
+              if(distance > 4){
+                   // set moving speed value
+                   this.scrollingMap.movingSpeed = distance * speedMult;
+                   // set moving angle value
+                   this.scrollingMap.movingangle = angle;
+              }
+         }
+	 }
 };
 
 var repeatConut;
@@ -55,7 +106,7 @@ Worldmap.prototype.makeWorldMap = function() {
 	var temp = this.game.make.bitmapData(this.game.width, 403);
 	worldmapBmd = this.game.make.bitmapData(this.game.width, 403*(repeatConut));
 
-	for(var i =5; i < repeatConut - 4; i++){
+	for(var i =4; i <= repeatConut; i++){
 		var str = 'worldmap'+(5+((i-1)%4));
 		temp.copy(str);
 		worldmapBmd.draw(temp,0,  403*(repeatConut-i));
@@ -79,7 +130,35 @@ Worldmap.prototype.makeWorldMap = function() {
 	
 	this.scrollingMap = this.game.add.image(0, 0, worldmapBmd);
 	this.scrollingMap.y = -403*(repeatConut-2);
+	
+	 this.scrollingMap.inputEnabled = true;
+     // map can be dragged
+     this.scrollingMap.input.enableDrag(false);
+     // custom property: we save map position
+     this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+     // custom property: the map is not being dragged at the moment
+     this.scrollingMap.isBeingDragged = false; 
+     // custom property: map is not moving (or is moving at no speed)
+     this.scrollingMap.movingSpeed = 0; 
+     // map can be dragged only if it entirely remains into this rectangle
+     this.scrollingMap.input.boundsRect = new Phaser.Rectangle(this.game.width - this.scrollingMap.width, 
+    		 this.game.height - this.scrollingMap.height, this.scrollingMap.width * 2 - this.game.width, 
+    		 this.scrollingMap.height * 2 - this.game.height);
+     // when the player starts dragging...
+     this.scrollingMap.events.onDragStart.add(function(){
+          // set isBeingDragged property to true
+          this.scrollingMap.isBeingDragged = true;
+          // set movingSpeed property to zero. This will stop moving the map
+          // if the player wants to drag when it's already moving
+          this.scrollingMap.movingSpeed = 0;
+     }, this);
+     // when the player stops dragging...
+     this.scrollingMap.events.onDragStop.add(function(){
+          // set isBeingDragged property to false
+          this.scrollingMap.isBeingDragged = false;
+     }, this);
 };
+
 Worldmap.prototype.stageDataArray = [];
 Worldmap.prototype.worldMapPointArray = [{x:243, y:334} ,{x:221, y:230} ,{x:218, y:98}, {x:225, y:31}, //2
                                       {x:159, y:356}, {x:324, y:356}, {x:309, y:162}, {x:172, y:67}, //3
@@ -113,17 +192,6 @@ Worldmap.prototype.makeButton = function() {
 		
 		this.scrollingMap.addChild(button);
 	}
-	
-//    
-//	var button1 = this.game.add.button(150, 100, 'disableStage.png', 
-//			null, this, 2, 1, 0);
-//	
-//	var button2 = this.game.add.button(200, 200, 'disableStage.png', 
-//			null, this, 2, 1, 0);
-//	
-//	this.scrollingMap.addChild(button);
-//	this.scrollingMap.addChild(button1);
-//	this.scrollingMap.addChild(button2);
 };
 
 Worldmap.prototype.setStageData = function(stageNum, stageData){
