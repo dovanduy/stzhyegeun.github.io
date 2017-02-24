b24530d25de075d282a018d132891232e3256431
stzhyegeun <hyegeun.cho@sundaytoz.com>
2017-02-24 15:53:24 +0900

    [오셀로] Peer API 키 추가

--- othelomulti/WebContent/lib/StzGameConfig.js
+++ othelomulti/WebContent/lib/StzGameConfig.js
@@ -1,71 +1,12 @@
 var StzGameConfig = {};
 
 // Preset Setting -- do not delete!!!
+
+// PROJECT SETTING
 StzGameConfig.DEBUG_MODE = true;
+
 StzGameConfig.LOCK_ORIENTATION = "PORTRAIT";
 //StzGameConfig.LOCK_ORIENTATION = "LANDSCAPE";
 
-// Custom Setting
-StzGameConfig.PRELOAD_BAR_MIN_WIDTH = 20;
-StzGameConfig.PRELOAD_BAR_MAX_WIDTH = 310;
-
-StzGameConfig.STAGE_PER_EPISODE = 20;
-StzGameConfig.TOTAL_EPISODE_COUNT = 5;
-
-StzGameConfig.BLOCK_WIDTH = 84;
-StzGameConfig.BLOCK_HEIGHT = 80;
-
-StzGameConfig.ROW_COUNT = 9;
-StzGameConfig.COL_COUNT = 9;
-
-// 인게임 블럭의 대분류
-var StzEBlockKind = {};
-StzEBlockKind.NONE = "ingame_none";
-StzEBlockKind.NORMAL = "ingame_block_0";
-StzEBlockKind.CAN = "ingame_block_10001";
-StzEBlockKind.WOOD_BOX = "ingame_block_10003";
-StzEBlockKind.MOLE = "ingame_block_10005";
-StzEBlockKind.SEED = "ingame_block_10006";
-StzEBlockKind.POPCORN = "ingame_block_20002";
-StzEBlockKind.MAO = "ingame_block_30007";
-StzEBlockKind.PINKY = "ingame_block_60001";
-StzEBlockKind.GUESS = "ingame_block_70001";
-
-StzEBlockKind.MISSION_DOGGUM = "ingame_block_mission_40001_0";
-StzEBlockKind.MISSION_EGG = "ingame_block_mission_40002";
-
-StzEBlockKind.SPECIAL_LASER = "ingame_special_2002";
-StzEBlockKind.SPECIAL_BOMB = "ingame_special_2004";
-
-var StzEBlockColor = {};
-StzEBlockColor.NONE = "";
-StzEBlockColor.YELLOW = "1";
-StzEBlockColor.RED = "2";
-StzEBlockColor.WHITE = "3";
-StzEBlockColor.GREEN = "4";
-StzEBlockColor.BLUE = "5";
-StzEBlockColor.ORANGE = "6";
-StzEBlockColor.MAX = "6";
-
-
-// 인게임 블럭의 Kind에 따른 소분류
-var StzEBlockKindType = {};
-StzEBlockKindType.NONE = "";
-StzEBlockKindType.NORMAL_LINE = "2002";
-StzEBlockKindType.NORMAL_BOMB = "2004";
-StzEBlockKindType.NORMAL_THUNDER = "2005";
-
-StzEBlockKindType.CAN_FIRST = "1";
-StzEBlockKindType.CAN_SECOND = "2";
-StzEBlockKindType.CAN_THIRD = "3";
-
-StzEBlockKindType.PINKY_NORMAL = "0";
-StzEBlockKindType.PINKY_HAPPY = "1";
-
-StzEBlockKindType.EGG_NORMAL = "0";
-StzEBlockKindType.EGG_CRACK = "1";
-
-var StzEBlockLineType = {};
-StzEBlockLineType.LINE_VERTICAL = "0";
-StzEBlockLineType.LINE_HORIZONTAL = "1";
+StzGameConfig.PEER_API_KEY = "e3sltd1aarod2t9";
 
