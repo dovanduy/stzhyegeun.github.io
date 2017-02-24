293ec63e5e8e3131ec7df25a0af522e6989a49cb
YooYoungSun <youngsun.yoo@sundaytoz.com>
2017-02-24 18:29:14 +0900

    [사천성] 블럭 구현 및 클릭 이벤트 구현

--- Sachunsung/WebContent/js/inGame/block.js
+++ Sachunsung/WebContent/js/inGame/block.js
@@ -6,13 +6,23 @@
 
 Phaser.Plugin.block = function(inGame, aParent, posBlock) {
 	Phaser.Plugin.call(this, inGame, aParent);
+	this.inGame = inGame;
+	this.aParent = aParent;
+	
+	this.isClicked = false;
 	
 	this.posBlock = posBlock;
-	this.block = inGame.add.sprite(0, 0, 'block', 'blankBlock.png');
-	this.blockIcon = inGame.add.sprite(0, 0, 'block', 'readyBlock.png');
-
+	
+	this.block = this.inGame.add.sprite(0, 0, 'block', 'blankBlock.png');
+	this.blockIcon = this.inGame.add.sprite(0, 0, 'block', 'readyBlock.png');
+	this.blockLine = this.inGame.add.sprite(0, 0, 'block', 'clicekBlock.png');
+	this.blockLine.visible = false;
+	
 	var posY = Math.floor(this.posBlock/10);
 	var posX = this.posBlock%10;
+	
+	this.block.inputEnabled = true;
+	this.block.events.onInputDown.add(this.onClickBlock, this);
 	
 	this.block.width = StzGameConfig.BLOCK_WIDTH;
 	this.block.height = StzGameConfig.BLOCK_HEIGHT;
@@ -22,23 +32,61 @@ Phaser.Plugin.block = function(inGame, aParent, posBlock) {
 	
 	aParent.scene.fGroupUI.add(this.block);
 	aParent.scene.fGroupUI.bringToTop(this.block);
+	
 	this.block.addChild(this.blockIcon);
+	this.block.addChild(this.blockLine);
 };
 
 Phaser.Plugin.block.prototype = {
+		inGame:null,
+		aParent:null,
 		pattern:0,
 		option:0,
 		posBlock:0,
 		blockType:0,
 		blockIcon:null,
-		block:null
+		blockLine:null,
+		block:null,
+		isClicked:false
 };
 
 Phaser.Plugin.block.prototype = Object.create(Phaser.Plugin.prototype);
 Phaser.Plugin.block.prototype.constructor = Phaser.Plugin.block;
 
+Phaser.Plugin.block.prototype.onClickBlock = function(){
+	if(this.isClicked === true){
+		this.blockIcon.frameName = 'animal_'+this.blockType+"_1.png";
+		this.isClicked = false;
+		this.blockLine.visible = false;
+	}
+	else{
+		this.blockIcon.frameName = 'animal_'+this.blockType+"_2.png";
+		this.isClicked = true;
+		this.blockLine.visible = true;
+	}
+	
+};
+
 Phaser.Plugin.block.prototype.readyBlockShow = function(){
 	this.blockIcon.frameName = 'readyBlock.png';
+};
+
+Phaser.Plugin.block.prototype.startBlockShow = function(){
+	this.blockIcon.frameName = 'animal_'+this.blockType+"_1.png";
+};
+
+Phaser.Plugin.block.prototype.setBlockPos = function(posBlock){
+	this.posBlock = posBlock;
+	
+	var posY = Math.floor(this.posBlock/10);
+	var posX = this.posBlock%10;
+	
+	this.block.x = StzGameConfig.BLOCK_MARGIN_X + (posX*this.block.width);
+	this.block.y = StzGameConfig.BLOCK_MARGIN_Y + (posY*this.block.height);
+};
+
+Phaser.Plugin.block.prototype.getBlockPos = function(){
+	return this.posBlock;
 };
 
 Phaser.Plugin.block.prototype.setBlockType = function(blockType){
