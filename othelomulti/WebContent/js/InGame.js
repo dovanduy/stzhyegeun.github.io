4d33fdfb3fcbe6c203142c023eb8e18e678f6fff
stzhyegeun <hyegeun.cho@sundaytoz.com>
2017-02-24 19:41:17 +0900

    [오셀로] Boot 씬 추가

---
+++ othelomulti/WebContent/js/InGame.js
@@ -0,0 +1,33 @@
+function InGame() {
+	Phaser.State.call(this);
+}
+
+var proto = Object.create(Phaser.state);
+InGame.prototype = proto;
+
+InGame.prototype.init = function() {
+	
+	this.board = StzCommon.StzUtil.createArray(StzGameConfig.ROW_COUNT, StzGameConfig.COL_COUNT);
+	this.currentTurn = ETurn.BLACK;
+};
+
+InGame.prototype.preload = function() {
+	
+};
+
+InGame.prototype.create = function() {
+	this.scene = new InGameScene(this.game);
+};
+
+InGame.prototype.initBoard = function() {
+	
+	var result = StzCommon.StzUtil.createArray(StzGameConfig.ROW_COUNT, StzGameConfig.COL_COUNT);
+};
+
+
+
+
+function ChipData(inType, inRow, inCol) {
+	this.chipType = inType;
+	this.chipIndex
+}
