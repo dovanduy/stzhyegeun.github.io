a7ce68c7dd015d544ba1c780b8a170f1210457e1
YooYoungSun <youngsun.yoo@sundaytoz.com>
2017-02-27 17:04:59 +0900

    [오셀로 인게임] 초기 chip 세팅

--- othelomulti/WebContent/js/InGame.js
+++ othelomulti/WebContent/js/InGame.js
@@ -23,11 +23,15 @@ InGame.prototype.initBoard = function() {
 	for (var rowIndex = 0; rowIndex < StzGameConfig.ROW_COUNT; rowIndex++) {
 		for (var colIndex = 0; colIndex < StzGameConfig.COL_COUNT; colIndex++) {
 			this.board[rowIndex][colIndex] = new Chip(this.game, this, rowIndex, colIndex);
-			this.board[rowIndex][colIndex].changeState(EChipType.MINIWHITE);	
+			this.board[rowIndex][colIndex].changeState(EChipType.NONE);	
 		}
 	}
 	
-	
+	//오셀로 시작 할 경우 4개의 칩이 세팅되어 있는 부분
+	this.board[3][3].changeState(EChipType.WHITE);
+	this.board[3][4].changeState(EChipType.BLACK);
+	this.board[4][3].changeState(EChipType.BLACK);
+	this.board[4][4].changeState(EChipType.WHITE);
 };
 
 function ChipData(inType, inRow, inCol) {
