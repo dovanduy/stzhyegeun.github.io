3a999d15113a2054be75b29349d50c4e64be7b8d
YooYoungSun <youngsun.yoo@sundaytoz.com>
2017-02-27 17:00:22 +0900

    [오셀로 인게임] chip.js 추가

---
+++ othelomulti/WebContent/js/chip/Chip.js
@@ -0,0 +1,44 @@
+Chip.prototype = {
+		inGame:null,
+		aParent:null,
+		posX:0,
+		posY:0,
+		chip:null
+};
+
+function Chip(ingame, aParent, rowIndex, colIndex) {
+	this.inGame = ingame;
+	this.aParent = aParent;
+	
+	this.chip = this.inGame.add.sprite(0, 0, 'inGameUI', 'blackChipMini.png');
+	this.chip.visible = false;
+	
+	this.posX = rowIndex*StzGameConfig.CHIP_WIDTH + StzGameConfig.CHIP_WIDTH/2;
+	this.posY = colIndex*StzGameConfig.CHIP_HEIGHT + StzGameConfig.CHIP_HEIGHT/2 + StzGameConfig.BOARD_TOP_OFFSET;
+	
+	this.chip.x = this.posX;
+	this.chip.y = this.posY;
+	
+	this.chip.anchor.set(0.5, 0.5);
+	
+	this.chip.inputEnabled = true;
+	this.chip.events.onInputUp.add(this.onClickBlock, this);
+	
+	this.aParent.scene.fGroupChip.add(this.chip);
+}
+
+Chip.prototype.changeState = function(state){
+	StzCommon.StzLog.print("[Chip changeState]");
+	
+	if(state === EChipType.NONE){
+		this.chip.visible = false;
+	}
+	else{
+		this.chip.visible = true;
+		this.chip.frameName = state;
+	}
+};
+
+Chip.prototype.onClickBlock = function(){
+	StzCommon.StzLog.print("[Chip onClickBlock]");
+};
