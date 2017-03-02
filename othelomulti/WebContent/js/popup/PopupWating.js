52e9498d8a0443bb1ee711dbe0465e4d146344bd
YooYoungSun <youngsun.yoo@sundaytoz.com>
2017-03-02 16:58:21 +0900

    [오셀로 인게임] Wating 팝업창 구현

---
+++ othelomulti/WebContent/js/popup/PopupWating.js
@@ -0,0 +1,22 @@
+function PopupWating(ingame, aParent) {
+	this.scene = new watingPopup(ingame);
+	Phaser.Plugin.PopupManager.apply(this, arguments);
+
+}
+
+PopupWating.prototype = Object.create(Phaser.Plugin.PopupManager.prototype);
+PopupWating.prototype.constructor = PopupWating;
+
+PopupWating.prototype.setData = function(){
+	
+};
+
+PopupWating.prototype.update = function(){
+	//Phaser.Plugin.PopupManager.prototype.update();
+	if(this.updateEnable === false ){
+		return;
+	}
+	
+	StzCommon.StzLog.print("[PopupResult] update");
+	
+};
